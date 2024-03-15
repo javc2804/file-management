2c9dda04b244067424ccf7d6e4d13b6a8d88c3c9
javier <javc2804@gmail.com>
2024-03-15 11:42:39 -0400

    estructura inicial, con login, recovery y forgot

---
+++ helpers/email.js
@@ -0,0 +1,64 @@
+import nodemailer from "nodemailer";
+
+export const emailRegistro = async (datos) => {
+  const { email, nombre, token } = datos;
+
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  // Información del email
+
+  const info = await transport.sendMail({
+    from: '"file management - gestor de archivos" <cuentas@management.com>',
+    to: email,
+    subject: "management - Comprueba tu cuenta",
+    text: "Comprueba tu cuenta en management",
+    html: `<p>Hola: ${nombre} Comprueba tu cuenta en management</p>
+    <p>Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace: 
+
+    <a href="${process.env.BACKEND_URL}/api/usuarios/confirmar/${token}">Comprobar Cuenta</a>
+    
+    <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
+    
+    
+    `,
+  });
+};
+
+export const emailOlvidePassword = async (datos) => {
+  const { email, nombre, token } = datos;
+
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  // Información del email
+
+  const info = await transport.sendMail({
+    from: '"management - gestor de archivos" <cuentas@management.com>',
+    to: email,
+    subject: "management - Reestablece tu Password",
+    text: "Reestablece tu Password",
+    html: `<p>Hola: ${nombre} has solicitado reestablecer tu password</p>
+
+    <p>Sigue el siguiente enlace para generar un nuevo password: 
+
+    <a href="${process.env.BACKEND_URL}/usuarios/olvide-password/${token}">Reestablecer Password</a>
+    
+    <p>Si tu no solicitaste este email, puedes ignorar el mensaje</p>
+    
+    
+    `,
+  });
+};
