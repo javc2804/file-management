2e406147985ff0767373dd5c3dbb82c0fa8d3a62
javier <javc2804@gmail.com>
2024-03-16 20:11:28 -0400

    proyecto finalizado

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routing
+
 app.use("/usuarios", usuarioRoutes);
 app.use("/files", fileRoutes);
 app.use("/images", imageRoutes);
